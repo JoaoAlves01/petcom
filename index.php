099f291a0c61bc36a2af0d68b21c6c07d312f5dc
Joao Alves <joaoalvesjc16@gmail.com>
2025-02-16 22:05:23 -0300

    feat: Adding HTML structure

---
+++ index.php
@@ -0,0 +1,123 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Petcom</title>
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">  
+  <link rel="stylesheet" href="assets/fontawesome/css/fontawesome.min.css">
+  <link rel="stylesheet" href="assets/fontawesome/css/solid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <header class="header">
+        <div class="header__container container">
+            <div class="header__row row">
+                <div class="header__logo col-md-4">
+                    Petcom
+                </div>
+                <div class="header__menu-col col-md-8">
+                    <ul class="header__menu menu">
+                        <li class="menu__item">Home</li>
+                        <li class="menu__item">About</li>
+                        <li class="menu__item">Service</li>
+                        <li class="menu__item">Contract</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+    <section class="banner">
+        <div class="banner__container container">
+            <div class="banner__row row">
+                <div class="banner__text col-md-12">
+                    Take a good care of pets
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="pet">
+        <div class="pet__container container">
+            <div class="pet__row row">
+                <div class="pet__card card col-md-4">
+                   <div class="card__pet card__pet--green">
+                        <img class="card__pet__image card__pet__image--contain" src="assets/images/pet-1.png" alt="image-pet-card-1">
+                   </div>
+                </div>
+                <div class="pet__card card col-md-4">
+                    <div class="card__pet card__pet--yellow">
+                         <img class="card__pet__image card__pet__image--contain" src="assets/images/pet-2.png" alt="image-pet-card-2">
+                    </div>
+                 </div>
+                 <div class="pet__card card col-md-4">
+                    <div class="card__pet card__pet--orange">
+                         <img class="card__pet__image card__pet__image--contain" src="assets/images/pet-3.png" alt="image-pet-card-3">
+                    </div>
+                 </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="about">
+        <div class="about__container container">
+            <div class="about__row row">
+                <div class="about__col col-md-6">
+                    <h2>Welcome to the Pet Care Company</h2>
+                    <p>
+                    Minions ipsum gelatooo uuuhhh para tú bappleees para tú tank yuuu! 
+                    Gelatooo po kass. Bappleees poopayee tulaliloo pepete belloo! Wiiiii. 
+                    Baboiii hana dul sae bappleees pepete hana dul sae po kass po kass baboiii. 
+                    Belloo! hahaha baboiii poopayee hahaha belloo! La bodaaa bee do bee do bee do chasy. 
+                    Pepete poopayee tank yuuu! Butt la bodaaa wiiiii aaaaaah ti aamoo! Poulet tikka masala. 
+                    Tatata bala tu daa ti aamoo! Poulet tikka masala poopayee wiiiii bappleees 
+                    hana dul sae ti aamoo! Jeje belloo!
+                    </p>
+                </div>
+                <div class="about__col col-md-6"></div>
+            </div>
+        </div>
+    </section>
+
+    <section class="service">
+        <div class="service__container container">
+            <div class="service__row row">
+                <div class="service__col col-md-12">
+                    <h2>We are best in:</h2>
+                    <p>
+                    Minions ipsum gelatooo uuuhhh para tú bappleees para tú tank yuuu! 
+                    Gelatooo po kass. Bappleees poopayee tulaliloo pepete belloo! Wiiiii. 
+                    Baboiii hana dul sae bappleees pepete hana dul sae po kass po kass baboiii. 
+                    Belloo! hahaha baboiii poopayee hahaha belloo! La bodaaa bee do bee do bee do chasy. 
+                    Pepete poopayee tank yuuu! Butt la bodaaa wiiiii aaaaaah ti aamoo! Poulet tikka masala. 
+                    Tatata bala tu daa ti aamoo! Poulet tikka masala poopayee wiiiii bappleees 
+                    hana dul sae ti aamoo! Jeje belloo!
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="contract">
+        <div class="contract__container container">
+            <div class="contract__row row">
+                <div class="contract__col col-md-12">
+                    <h2>Book an appoiment</h2>
+                    <p>
+                    Minions ipsum gelatooo uuuhhh para tú bappleees para tú tank yuuu! 
+                    Gelatooo po kass. Bappleees poopayee tulaliloo pepete belloo! Wiiiii. 
+                    Baboiii hana dul sae bappleees pepete hana dul sae po kass po kass baboiii. 
+                    Belloo! hahaha baboiii poopayee hahaha belloo! La bodaaa bee do bee do bee do chasy. 
+                    Pepete poopayee tank yuuu! Butt la bodaaa wiiiii aaaaaah ti aamoo! Poulet tikka masala. 
+                    Tatata bala tu daa ti aamoo! Poulet tikka masala poopayee wiiiii bappleees 
+                    hana dul sae ti aamoo! Jeje belloo!
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer></footer>
+</body>
+</html>
